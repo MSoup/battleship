3bd0fafdb420731003aea4b826e4b168fb17ca1e
Daven <davenlu1989@gmail.com>
2022-08-06 14:04:13 +0900

    progress save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Board from './components/Board'
 import GameMenu from './components/GameMenu'
+import Dropdown from './components/Dropdown'
 
 function App() {
   return (
     <div className="App">
-      <h1>Battleship MVP</h1>
       <header className="App-header">
+        <Dropdown />
       </header>
+      <h1>Battleship MVP</h1>
         <GameMenu />
     </div>
   );
---
+++ src/components/Dropdown.js
@@ -0,0 +1,11 @@
+export default function Dropdown() {
+    return (
+        <nav>
+            <ul>
+                <li class="new-game">New Game</li>
+                <li class="cta">Challenge Friend</li>   
+                <li class="mlink">Undo</li>
+            </ul>
+      </nav>
+    );
+}
