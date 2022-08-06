77973e83ffe5bcdc5bb057a601a2daaa844407ea
JC Orolfo <orolfojc@gmail.com>
2022-08-06 09:34:57 +0900

    changes default height and width to 5

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -4,8 +4,8 @@ import Board from "./Board"
 
 export default function GameMenu() {
     // defaults
-    const [width, setWidth] = useState(7)
-    const [height, setHeight] = useState(7)
+    const [width, setWidth] = useState(8)
+    const [height, setHeight] = useState(8)
 
     const newGameInit = () => {
         console.log("Init called")
